c94254dccc4c307d9e201f27e18c883d9b6f5497
CoCo Chanel <veta583518@gmail.com>
2021-04-29 20:26:15 -0500

    add untracked files

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,8 +4,12 @@ const userRoutes = require("./user-routes.js");
 
 const postRoutes = require("./post-routes");
 
+const commentRoutes = require("./comment-routes");
+
 router.use("/users", userRoutes);
 
 router.use("/posts", postRoutes);
+
+router.use("/comments", commentRoutes);
 
 module.exports = router;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
 // GET ROUTES -------------------------------------------------------------------------------------------------------
@@ -22,6 +22,14 @@ router.get("/", (req, res) => {
     ],
     order: [["created_at", "DESC"]],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -54,6 +62,14 @@ router.get("/:id", (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
