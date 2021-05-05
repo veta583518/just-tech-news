564260d785516331a795f4ab696406d438ded03a
CoCo Chanel <veta583518@gmail.com>
2021-05-04 23:44:57 -0500

    added user ability to add, edit and delete post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
 
 // GET ROUTES -------------------------------------------------------------------------------------------------------
 // get all users
@@ -90,12 +91,12 @@ router.get("/:id", (req, res) => {
 });
 // POST ROUTES -----------------------------------------------------------------------------------------------------------------------
 // create a Post
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id,
+    user_id: req.session.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -106,7 +107,7 @@ router.post("/", (req, res) => {
 
 // PUT ROUTES ----------------------------------------------------------------------------------------------------------
 // PUT /api/posts/upvote (create a vote)
-router.put("/upvote", (req, res) => {
+router.put("/upvote", withAuth, (req, res) => {
   // custom static method created in models/Post.js
   // make sure the session exists first
   if (req.session) {
@@ -123,7 +124,7 @@ router.put("/upvote", (req, res) => {
   }
 });
 // update a post's title
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -148,7 +149,7 @@ router.put("/:id", (req, res) => {
 });
 
 // delete a post
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User, Post, Vote } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // get all users
 router.get("/", (req, res) => {
@@ -53,7 +54,7 @@ router.get("/:id", (req, res) => {
     });
 });
 // create a new user
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
@@ -71,7 +72,7 @@ router.post("/", (req, res) => {
 });
 
 // login route
-router.post("/login", (req, res) => {
+router.post("/login", withAuth, (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
@@ -103,7 +104,7 @@ router.post("/login", (req, res) => {
 });
 
 // Logout route
-router.post("/logout", (req, res) => {
+router.post("/logout", withAuth, (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -114,7 +115,7 @@ router.post("/logout", (req, res) => {
 });
 
 // update user by id
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
@@ -137,7 +138,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   User.destroy({
     where: {
       id: req.params.id,
